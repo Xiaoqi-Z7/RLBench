7c761eb6a3d858573d6eaf8e72a27bf6f3401a3d
xiaoqi <xiaoqizhou77@gmail.com>
2026-03-09 18:08:44 +0100

    add gt for lift ball task

--- rlbench/bimanual_tasks/bimanual_lift_ball.py
+++ rlbench/bimanual_tasks/bimanual_lift_ball.py
@@ -48,3 +48,8 @@ class BimanualLiftBall(BimanualTask):
 
     def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
         return [0, 0, - np.pi / 8], [0, 0, np.pi / 8]
+    
+    def get_obj_poses(self):
+        poses = {}
+        poses['ball'] = self.ball.get_pose()
+        return poses
